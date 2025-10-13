4a57c4af160a4f8a895f738d718ffcb311c9ab49
tanvirabsar <tjoy4799@gmail.com>
2025-10-13 23:43:58 +0600

    approxcimitly complate

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,9 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-    <div className='rounded-3xl flex gap-3 items-center w-1/2 mx-auto justify-between p-1 sticky top-0'>
+
+      <div className='w-full  flex justify-center items-center py-5 '>
+        <div className='rounded-3xl flex gap-3 items-center   justify-center p-1 absolute s bg-white mx-auto  top-0'>
         <div>
             <Image height={40} width={40} alt='logo' src={'/ryven_co_logo 1.png'}></Image>
         </div>
@@ -17,5 +19,7 @@ export default function NavBar() {
             <button className=''>Linkedin</button>
         </div>
     </div>
+      </div>
+   
   )
 }
